6dc8b0065466e38e09d97ef83fe040ebd5b96110
Leandro Barros <leandroedbarros@gmail.com>
2022-07-18 19:46:37 -0300

    Creating service CashBack-service

---
+++ cashback-service/src/main/java/com/example/springamqp/CashBackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashBackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashBackServiceApplication.class, args);
+	}
+
+}
---
+++ cashback-service/src/test/java/com/example/springamqp/CashBackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springamqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashBackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
