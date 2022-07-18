9930ace070a1f1366a5ce3c496d4f226fb7527e0
Leandro Barros <leandroedbarros@gmail.com>
2022-07-18 20:39:29 -0300

    Feature: Creating class repository order

---
+++ order-service/src/main/java/com/example/springamqp/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.springamqp.repository;
+
+import org.hibernate.criterion.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
