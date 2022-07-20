cb65cd46300b7451f913964649095bb526101c35
Leandro Barros <leandroedbarros@gmail.com>
2022-07-19 22:51:47 -0300

    Producer first message in rabbitMQ

--- order-service/src/main/java/com/example/springamqp/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/example/springamqp/service/impl/OrderServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.springamqp.service.impl;
 import com.example.springamqp.model.OrderModel;
 import com.example.springamqp.repository.OrderRepository;
 import com.example.springamqp.service.OrderService;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,17 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Override
     public OrderModel save(OrderModel orderModel) {
-        return orderRepository.save(orderModel);
+        OrderModel orderModelSaved = orderRepository.save(orderModel);
+
+        String routingKey = "orders.v1.order-created";
+        Message message = new Message(orderModelSaved.getId().toString().getBytes());
+        rabbitTemplate.send(routingKey, message);
+
+        return orderModelSaved;
     }
 }
