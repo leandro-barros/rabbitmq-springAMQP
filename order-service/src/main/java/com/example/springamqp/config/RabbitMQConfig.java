8586ccc90aafbb18c558cb55eb3ed96a85f6dd71
Leandro Barros <leandroedbarros@gmail.com>
2022-07-21 21:50:59 -0300

    Bugfix: Chaging name queue

--- order-service/src/main/java/com/example/springamqp/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/example/springamqp/config/RabbitMQConfig.java
@@ -15,7 +15,7 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue queue() {
-        return new Queue("${queue.order-created}");
+        return new Queue("orders.v1.order-created");
     }
 
     @Bean
