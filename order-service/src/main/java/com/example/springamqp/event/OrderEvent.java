d1a715982d0f30003fe26b335bd4a2bd94586455
Leandro Barros <leandroedbarros@gmail.com>
2022-07-23 10:25:36 -0300

    Feature: Send message to exchange fanout

--- order-service/src/main/java/com/example/springamqp/event/OrderEvent.java
+++ order-service/src/main/java/com/example/springamqp/event/OrderEvent.java
@@ -5,6 +5,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
 public class OrderEvent {
 
@@ -14,6 +16,11 @@ public class OrderEvent {
     public void sendMessage(OrderEventDto orderEventDto) {
         rabbitTemplate.convertAndSend("orders.v1.order-created.generate-cashback", orderEventDto);
         rabbitTemplate.convertAndSend("orders.v1.order-created.send-notification", orderEventDto);
+    }
+
+    public void sendMessageExchange(OrderEventDto orderEventDto) {
+        orderEventDto.setValue(BigDecimal.valueOf(100));
+        rabbitTemplate.convertAndSend("orders.v1.order-created", "", orderEventDto);
     }
 
 }
