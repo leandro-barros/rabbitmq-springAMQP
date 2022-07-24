a2d07d398619761c0712bea3892b34796f826bd1
Leandro Barros <leandroedbarros@gmail.com>
2022-07-24 09:38:03 -0300

    Feature: Created binding betwenn cashback queue and exchange fanout

--- cashback-service/src/main/java/com/example/springamqp/config/RabbitMQConfig.java
+++ cashback-service/src/main/java/com/example/springamqp/config/RabbitMQConfig.java
@@ -1,9 +1,15 @@
 package com.example.springamqp.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +21,20 @@ public class RabbitMQConfig {
         return new Queue("orders.v1.order-created.generate-cashback");
     }
 
+    @Bean
+    public Binding binding() {
+        FanoutExchange fanoutExchange = new FanoutExchange("orders.v1.order-created");
+        return BindingBuilder.bind(queueCashback()).to(fanoutExchange);
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener (RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
 
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
