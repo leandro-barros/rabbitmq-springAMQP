9574b1a65d375a78bef5e2c45628d3370033b446
Leandro Barros <leandroedbarros@gmail.com>
2022-07-24 09:52:23 -0300

    Feature: Creating link between notification queue and fanout exchange

--- notification-service/src/main/java/com/example/springamqp/config/RabbitMQConfig.java
+++ notification-service/src/main/java/com/example/springamqp/config/RabbitMQConfig.java
@@ -1,9 +1,15 @@
 package com.example.springamqp.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +19,21 @@ public class RabbitMQConfig {
     @Bean
     public Queue queueNotification() {
         return new Queue("orders.v1.order-created.send-notification");
+    }
+
+    @Bean
+    public Binding binding() {
+        FanoutExchange fanoutExchange = new FanoutExchange("orders.v1.order-created");
+        return BindingBuilder.bind(queueNotification()).to(fanoutExchange);
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener (RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
     }
 
     @Bean
